45ab9cc77324019ce49670e8f98d436de8fea509
Andrew Waldron <awaldro@gmail.com>
2017-04-12 19:25:10 -0400

    Trying to figure out IoT in AWS. Hm

--- iot/index.js
+++ iot/index.js
@@ -1,6 +1,42 @@
 var iot = require('aws-iot-device-sdk');
 
-var deviceId = 'a93bd9538a';
-var device= iot.device({
-  keyPath: 'certificate/'
+const device = iot.device({
+  keyPath: 'certificate/LightBulb.private.key',
+  certPath: 'certificate/LightBulb.cert.pem',
+  caPath: 'certificate/root-CA.crt',
+  region: 'us-east-1',
+  host: 'a1vb512hpb4stb.iot.us-east-1.amazonaws.com',
+  debug: false
+
+  // TODO: Figure out the hopefully optional parameters
+  // clientId: args.clientId,
+  // baseReconnectTimeMs: args.baseReconnectTimeMs,
+  // keepalive: args.keepAlive,
+  // protocol: args.Protocol,
+  // port: args.Port,
 });
+
+device
+  .on('connect', function() {
+     console.log('connect');
+  });
+device
+  .on('close', function() {
+     console.log('close');
+  });
+device
+  .on('reconnect', function() {
+     console.log('reconnect');
+  });
+device
+  .on('offline', function() {
+     console.log('offline');
+  });
+device
+  .on('error', function(error) {
+     console.log('error', error);
+  });
+device
+  .on('message', function(topic, payload) {
+     console.log('message', topic, payload.toString());
+  });
