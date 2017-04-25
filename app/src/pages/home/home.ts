98500785bea916a48c07cfb5dc4012a4e2851aa4
Andrew Waldron <awaldro@gmail.com>
2017-04-25 00:17:51 -0400

    Don't really know everything that had to happen but it was lots more than I had bargained on

--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -2,30 +2,33 @@ import { Component, NgZone } from '@angular/core';
 import { NavController, Platform, Events } from 'ionic-angular';
 import { Buffer } from 'buffer';
 
-import { BeaconProvider } from '../../providers/beacon-provider';
-import { BeaconModel } from '../../models/beacon-model';
-import * as { AWS } from '../../browser';
+// import { BeaconProvider } from '../../providers/beacon-provider';
+// import { BeaconModel } from '../../models/beacon-model';
+import * as AWS from 'aws-sdk';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  beacons: BeaconModel[] = [];
+  // beacons: BeaconModel[] = [];
   zone: NgZone;
   debug_messages: string;
   isConnected: boolean;
   device: any;
   topic: string = 'light-control';
 
-  constructor(public navCtrl: NavController, public platform: Platform, public beaconProvider: BeaconProvider, public events: Events) {
+  // constructor(public navCtrl: NavController, public platform: Platform, public beaconProvider: BeaconProvider, public events: Events) {
+  constructor(public navCtrl: NavController, public platform: Platform, public events: Events) {
   	this.debug_messages = "No Debug Message";
     this.zone = new NgZone({ enableLongStackTrace: false });
 
+    console.log(AWS);
+
     var config = new AWS.Config({
       credentials: new AWS.Credentials({
-        accessKeyId: '<access_key_id>',
-        secretAccessKey: '<access_key>'
+        accessKeyId: '<access key>',
+        secretAccessKey: '<access key key>'
       }),
       region: 'us-east-1'
     });
@@ -62,13 +65,13 @@ export class HomePage {
     this.platform.ready().then(() => {
   	  this.debug_messages = "Starting to initialize";
 
-      this.beaconProvider.initialize().then((isInitialized) => {
-  	  	this.debug_messages = "initialize complete";
+      // this.beaconProvider.initialize().then((isInitialized) => {
+  	  	// this.debug_messages = "initialize complete";
 
-        if (isInitialized) {
-          this.listenToBeaconEvents();
-        }
-      });
+      //   if (isInitialized) {
+      //     this.listenToBeaconEvents();
+      //   }
+      // });
     });
   }
 
@@ -76,22 +79,22 @@ export class HomePage {
     this.events.subscribe('didRangeBeaconsInRegion', (data) => {
   	  this.debug_messages = "got the beacons in region event";
 
-      this.zone.run(() => {
-        this.beacons = [];
-        let beaconList = data.beacons;
-        beaconList.forEach((beacon) => {
-          let beaconObject = new BeaconModel(beacon);
-          this.beacons.push(beaconObject);
+      // this.zone.run(() => {
+      //   this.beacons = [];
+      //   let beaconList = data.beacons;
+      //   beaconList.forEach((beacon) => {
+      //     let beaconObject = new BeaconModel(beacon);
+      //     this.beacons.push(beaconObject);
 
-          if (this.isConnected) {
-            if (Math.abs(beaconObject.rssi) < 70) {
-            	this.sendCommand('on');
-            } else {
-            	this.sendCommand('off');
-            }
-          }
-        });
-      });
+      //     if (this.isConnected) {
+      //       if (Math.abs(beaconObject.rssi) < 70) {
+      //       	this.sendCommand('on');
+      //       } else {
+      //       	this.sendCommand('off');
+      //       }
+      //     }
+      //   });
+      // });
     });
   }
 }
